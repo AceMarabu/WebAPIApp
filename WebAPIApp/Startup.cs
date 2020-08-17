af3577bee757617c8f97d6f60936215417457e7c
klim1880@gmail.com <klim1880@gmail.com>
2020-08-17 23:56:16 +0300

    first commit

---
+++ WebAPIApp/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebAPIApp.Models;
+
+namespace WebAPIApp
+{
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			string con = "Server=(localdb)\\mssqllocaldb;Database=usersdbstore;Trusted_Connection=True;";
+			// устанавливаем контекст данных
+			services.AddDbContext<UsersContext>(options => options.UseSqlServer(con));
+
+			services.AddControllers(); // Использование контроллеров без представлений (Using controllers without views)
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseDeveloperExceptionPage();
+
+			app.UseRouting();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers(); // Подключение маршрутизации к контроллерам (Connecting routing to controllers)
+			});
+		}
+	}
+}
